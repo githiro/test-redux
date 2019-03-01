05d8714b077807744d20ab1247d94d651258b215
yoshimatsu <jpc.yoshimatsu@gmail.com>
2019-03-01 15:34:40 +0900

    Providerを作成しindex.jsに指定

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux'
+import { Provider } frome 'react-redux'// 作成したStoreを全Componentにくばるためのもの
+
 import './index.css';
-import App from './App';
+import reducer from './reducers'
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducer)// すべてのreducerを１箇所にまとめる
+
+
+ReactDOM.render(
+	// Providerを使うことで、すべてに対してreducerで整形したstateが分配され、どこからでも参照できるようになる
+	// つまりcomponentのpropsを使って値をバケツリレーする必要がなくなる
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
